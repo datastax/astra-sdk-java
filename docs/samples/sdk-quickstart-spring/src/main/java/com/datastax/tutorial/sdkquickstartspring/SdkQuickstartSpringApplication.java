57b66e822fd02d8f0806ab190472632599f74591
clun <cedrick.lunven@gmail.com>
2021-11-01 14:47:08 +0100

    new pic

--- docs/samples/sdk-quickstart-spring/src/main/java/com/datastax/tutorial/sdkquickstartspring/SdkQuickstartSpringApplication.java
+++ docs/samples/sdk-quickstart-spring/src/main/java/com/datastax/tutorial/sdkquickstartspring/SdkQuickstartSpringApplication.java
@@ -1,11 +1,14 @@
 package com.datastax.tutorial.sdkquickstartspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.datastax.astra.sdk.AstraClient;
 
 @RestController
 @SpringBootApplication(exclude = { CassandraDataAutoConfiguration.class, CassandraAutoConfiguration.class })
@@ -15,7 +18,12 @@ public class SdkQuickstartSpringApplication {
 		SpringApplication.run(SdkQuickstartSpringApplication.class, args);
 	}
 	
+	@Autowired
+	private AstraClient astraClient;
+	
 	@GetMapping("/")
-	public String hello() { return "OK"; }
+	public String hello() { 
+	    return astraClient.apiDevopsOrganizations().organizationId(); 
+	}
 
 }
