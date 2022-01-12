c1d4a8a18821e8bba9a4fc59c8eed993aff987a5
clun <cedrick.lunven@gmail.com>
2022-01-12 10:00:44 +0100

    Move samples in dedicated repository

--- docs/samples/sdk-quickstart-spring/src/main/java/com/datastax/tutorial/TodosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.datastax.tutorial;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface TodosRepository extends CassandraRepository<Todos, String> {
-
-}
