7dcb116470f8dbf1a771764936ddfffc832ca927
Cedrick Lunven <cedrick.lunven@gmail.com>
2023-12-08 16:01:32 +0100

    test

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ListCollections.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ListCollections.java
@@ -3,17 +3,17 @@ import com.dtsx.astra.sdk.AstraDB;
 import io.stargate.sdk.json.domain.CollectionDefinition;
 
 public class ListCollections {
-    public static void main(String[] args) {
-        // Given an active db
-        AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-        // Iterate over all collections
-        db.findAllCollections().forEach(col -> {
-            System.out.println("name=" + col.getName());
-            if (col.getOptions() != null && col.getOptions().getVector() != null) {
-                CollectionDefinition.Options.Vector vector = col.getOptions().getVector();
-                System.out.println("dim=" + vector.getDimension());
-                System.out.println("metric=" + vector.getMetric());
-            }
-        });
-    }
+  public static void main(String[] args) {
+    // Given an active db
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+    // Iterate over all collections
+    db.findAllCollections().forEach(col -> {
+      System.out.println("name=" + col.getName());
+      if (col.getOptions() != null && col.getOptions().getVector() != null) {
+        CollectionDefinition.Options.Vector vector = col.getOptions().getVector();
+        System.out.println("dim=" + vector.getDimension());
+        System.out.println("metric=" + vector.getMetric());
+      }
+    });
+  }
 }
