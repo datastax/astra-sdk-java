a254e458e4d96bd68f85d0932464db372fb67e04
Cedrick Lunven <cedrick.lunven@gmail.com>
2023-12-10 01:48:58 +0100

    docs

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/CreateCollection.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/CreateCollection.java
@@ -7,10 +7,8 @@ import io.stargate.sdk.json.domain.SimilarityMetric;
 public class CreateCollection {
   public static void main(String[] args) {
     // Given an active db
-    //AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-    AstraDB db = new AstraDB(
-            "AstraCS:iLPiNPxSSIdefoRdkTWCfWXt:2b360d096e0e6cb732371925ffcc6485541ff78067759a2a1130390e231c2c7a",
-            "https://1537fca7-e315-4a63-8773-846bde477518-us-east1.apps.astra.datastax.com");
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+
     // Create collection with no vector
     AstraDBCollection collection1 = db.createCollection("collection_simple");
 
--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/DeleteCollection.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/DeleteCollection.java
@@ -7,10 +7,8 @@ import java.util.Optional;
 public class DeleteCollection {
   public static void main(String[] args) {
     // Given an active db
-    //AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-    AstraDB db = new AstraDB(
-            "AstraCS:iLPiNPxSSIdefoRdkTWCfWXt:2b360d096e0e6cb732371925ffcc6485541ff78067759a2a1130390e231c2c7a",
-            "https://1537fca7-e315-4a63-8773-846bde477518-us-east1.apps.astra.datastax.com");
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+
     // Find a collection
     db.deleteCollection("tmp_collection");
   }
