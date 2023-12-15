9761374443df4492c797c873903b7d619968feba
Cedrick Lunven <cedrick.lunven@gmail.com>
2023-12-15 17:48:12 +0100

    Documentation Support

---
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/FindPage.java
@@ -0,0 +1,62 @@
+package com.dtsx.astra.sdk.documentation;
+
+import com.dtsx.astra.sdk.AstraDB;
+import com.dtsx.astra.sdk.AstraDBCollection;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.langchain4j.internal.Json;
+import io.stargate.sdk.core.domain.Page;
+import io.stargate.sdk.json.domain.JsonResult;
+import io.stargate.sdk.json.domain.SelectQuery;
+import io.stargate.sdk.json.domain.odm.Result;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindPage {
+    public static void main(String[] args) {
+
+// Accessing existing DB
+AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+
+// Access existing collection
+AstraDBCollection collection = db
+ .createCollection("collection_vector1", 14);
+
+// Retrieve page1 of a search
+Page<JsonResult> page1 = collection.findPage(SelectQuery.builder()
+ .where("product_price").isEqualsTo(9.99)
+ .build());
+
+// Retrieving page 2 of the same search if more than 20
+page1.getPageState().ifPresent(pageState -> {
+    Page<JsonResult> page2 = collection.findPage(SelectQuery.builder()
+     .where("product_price").isEqualsTo(9.99)
+     .withPagingState(pageState)
+     .build());
+});
+
+/*
+ * As for any find* you can map the output as Result<T>
+ * using either a java pojo or mapper.
+ */
+Page<Result<MyBean>> page = collection.findPage(SelectQuery.builder()
+ .where("product_price").isEqualsTo(9.99)
+ .build(), MyBean.class);
+
+}
+
+public static class MyBean {
+    @JsonProperty("product_name")
+    String name;
+    @JsonProperty("product_price")
+    Double price;
+
+    public MyBean(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+// getters and setters
+}
+}
---
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ObjectMappingCreateCollection.java
@@ -0,0 +1,41 @@
+package com.dtsx.astra.sdk.documentation;
+
+import com.dtsx.astra.sdk.AstraDB;
+import com.dtsx.astra.sdk.AstraDBCollection;
+import com.dtsx.astra.sdk.AstraDBRepository;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.stargate.sdk.json.domain.CollectionDefinition;
+import io.stargate.sdk.json.domain.SimilarityMetric;
+import io.stargate.sdk.json.exception.JsonApiException;
+
+public class ObjectMappingCreateCollection {
+
+  static class Product {
+
+    @JsonProperty("product_name")
+    private String name;
+
+    @JsonProperty("product_price")
+    private Double price;
+
+    // getters and setters
+  }
+
+  public static void main(String[] args) {
+    // Given an active db
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+
+    /*
+     * Create collection with no vector.
+     */
+    AstraDBRepository<Product> collection1 = db
+            .createCollection("collection_simple", Product.class);
+
+    // Create collection with vector (builder)
+    AstraDBRepository<Product> collection2 = db.createCollection(CollectionDefinition
+            .builder()
+            .name("collection_vector2")
+            .vector(1536, SimilarityMetric.euclidean)
+            .build(), Product.class);
+  }
+}
