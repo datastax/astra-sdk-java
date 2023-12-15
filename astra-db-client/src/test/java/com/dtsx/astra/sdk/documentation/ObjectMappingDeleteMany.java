e18133d60632476deef60daaee25c8535913f485
Daniel Sauble <djsauble@gmail.com>
2023-12-15 14:21:37 -0800

    Update ObjectMappingDeleteMany.java
    * Two-space indent
    * Remove trailing empty line
    * Reformat line wrapping

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ObjectMappingDeleteMany.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ObjectMappingDeleteMany.java
@@ -7,32 +7,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.stargate.sdk.json.domain.DeleteQuery;
 
 public class ObjectMappingDeleteMany {
+  static class Product {
+    @JsonProperty("product_name") private String name;
+    @JsonProperty("product_price") private Double price;
+  }
 
-    static class Product {
+  public static void main(String[] args) {
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
 
-        @JsonProperty("product_name")
-        private String name;
+    // Create a vector collection
+    AstraDBRepository<Product> collection1 =
+        db.createCollection("collection_simple", Product.class);
 
-        @JsonProperty("product_price")
-        private Double price;
-
-        // getters and setters
-    }
-
-    public static void main(String[] args) {
-
-// Given an active db and a collection with a vector field (see CreateCollection.java)
-AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-
-/*
- * Create collection with no vector.
- */
-AstraDBRepository<Product> collection1 = db
-  .createCollection("collection_simple", Product.class);
-
-// Delete item based on a query
-int deletedCount = collection1.deleteAll(DeleteQuery.builder()
-       .where("product_price").isEqualsTo(9.99)
-       .build());
-    }
+    // Delete rows based on a query
+    int deletedCount = collection1.deleteAll(
+        DeleteQuery.builder()
+            .where("product_price")
+            .isEqualsTo(9.99)
+            .build());
+  }
 }
