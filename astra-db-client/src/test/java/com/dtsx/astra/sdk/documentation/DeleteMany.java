3e426d314134e074f680d6f86c048bdb20b9eb36
Daniel Sauble <djsauble@gmail.com>
2023-12-15 10:30:57 -0800

    Update DeleteMany.java
    * Use a two-space indent
    * Remove comments that aren't core to the example
    * Reword the comment slightly

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/DeleteMany.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/DeleteMany.java
@@ -6,19 +6,15 @@ import com.dtsx.astra.sdk.AstraDBCollection;
 import io.stargate.sdk.json.domain.DeleteQuery;
 
 public class DeleteMany {
-public static void main(String[] args) {
+  public static void main(String[] args) {
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+    AstraDBCollection collection = db
+        .createCollection("collection_vector1",14);
 
-// Given an active db and a collection with a vector field (see CreateCollection.java)
-AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-
-// Create collection if not exists
-AstraDBCollection collection = db
-  .createCollection("collection_vector1",14);
-
-// Delete item based on a query
-int deletedCount = collection
- .deleteMany(DeleteQuery.builder()
- .where("product_price").isEqualsTo(9.99)
- .build());
-}
+    // Delete items from an existing collection with a query
+    int deletedCount = collection
+        .deleteMany(DeleteQuery.builder()
+        .where("product_price").isEqualsTo(9.99)
+        .build());
+  }
 }
