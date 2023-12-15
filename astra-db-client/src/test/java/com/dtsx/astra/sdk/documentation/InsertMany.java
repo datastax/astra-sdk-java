0967758f1cb9b3ec3b4d790cc9caed6221991aef
Daniel Sauble <djsauble@gmail.com>
2023-12-15 11:46:53 -0800

    Update InsertMany.java
    * Two-space indent
    * Remove non-core comments
    * Restructure line wrapping

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/InsertMany.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/InsertMany.java
@@ -3,29 +3,22 @@ package com.dtsx.astra.sdk.documentation;
 import com.dtsx.astra.sdk.AstraDB;
 import com.dtsx.astra.sdk.AstraDBCollection;
 import io.stargate.sdk.json.domain.JsonDocument;
-
 import java.util.List;
 
 public class InsertMany {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+    AstraDBCollection collection = db.createCollection("collection_vector1",14);
 
-// Given an active db and a collection with a vector field (see CreateCollection.java)
-AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-
-// Create collection if not exists
-AstraDBCollection collection = db
-        .createCollection("collection_vector1",14);
-
-// Insert documents, ids are generated here
-List<String> identifiers = collection.insertMany(List.of(
-  new JsonDocument()
-    .vector(new float[]{1f, 0f, 1f, 1f, .5f, 1f, 0f, 0.3f, 0f, 0f, 0f, 0f, 0f, 0f})
-    .put("product_name", "Yet another product")
-    .put("product_price", 99.99),
-  new JsonDocument()
-    .vector(new float[]{1f, 0f, 1f, 1f, .5f, 1f, 0f, 0.3f, 0f, 0f, 0f, 0f, 0f, 0f})
-    .put("product_name", "product3")
-    .put("product_price", 99.99)));
-
-}
+    // Insert documents into the collection (IDs are generated automatically)
+    List<String> identifiers = collection.insertMany(List.of(
+        new JsonDocument()
+            .vector(new float[]{1f, 0f, 1f, 1f, .5f, 1f, 0f, 0.3f, 0f, 0f, 0f, 0f, 0f, 0f})
+            .put("product_name", "Yet another product")
+            .put("product_price", 99.99),
+        new JsonDocument()
+            .vector(new float[]{1f, 0f, 1f, 1f, .5f, 1f, 0f, 0.3f, 0f, 0f, 0f, 0f, 0f, 0f})
+            .put("product_name", "product3")
+            .put("product_price", 99.99)));
+  }
 }
