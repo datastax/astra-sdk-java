43052f04084b1877060ca81a2bdc9007eb260569
Daniel Sauble <djsauble@gmail.com>
2023-12-15 14:45:13 -0800

    Update QuickStart.java

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/QuickStart.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/QuickStart.java
@@ -10,39 +10,43 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 public class QuickStart {
- public static void main(String[] args) {
+  public static void main(String[] args) {
 
-// Initialization
-AstraDB myDb = new AstraDB("<token>", "<api_endpoint>");
+    // Initialize the client
+    AstraDB myDb = new AstraDB("<token>", "<api_endpoint>");
 
-// Create a collection (if needed)
-AstraDBCollection demoCollection = myDb.createCollection("demo",14);
+    // Create a collection
+    AstraDBCollection demoCollection = myDb.createCollection("demo",14);
 
-// Insert vectors
-demoCollection.insertOne(new JsonDocument()
- .id("doc1") // generated if not set
- .vector(new float[]{1f, 0f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f})
- .put("product_name", "HealthyFresh - Beef raw dog food")
- .put("product_price", 12.99));
-demoCollection.insertOne(new JsonDocument()
- .id("doc2")
- .vector(new float[]{1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f})
- .put("product_name", "HealthyFresh - Chicken raw dog food")
- .put("product_price", 9.99));
-demoCollection.insertOne(new JsonDocument()
- .id("doc3")
- .vector(new float[]{1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f})
- .data(Map.of("product_name", "HealthyFresh - Chicken raw dog food")));
-demoCollection.insertOne(new JsonDocument()
- .id("doc4")
- .vector(new float[]{1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f})
- .put("product_name", "HealthyFresh - Chicken raw dog food")
- .put("product_price", 9.99));
+   // Insert vectors
+   demoCollection.insertOne(
+       new JsonDocument()
+           .id("doc1") // generated if not set
+           .vector(new float[]{1f, 0f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f})
+           .put("product_name", "HealthyFresh - Beef raw dog food")
+           .put("product_price", 12.99));
+    demoCollection.insertOne(
+        new JsonDocument()
+           .id("doc2")
+           .vector(new float[]{1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f})
+           .put("product_name", "HealthyFresh - Chicken raw dog food")
+           .put("product_price", 9.99));
+    demoCollection.insertOne(
+        new JsonDocument()
+           .id("doc3")
+           .vector(new float[]{1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f})
+           .data(Map.of("product_name", "HealthyFresh - Chicken raw dog food")));
+    demoCollection.insertOne(
+        new JsonDocument()
+           .id("doc4")
+           .vector(new float[]{1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f})
+           .put("product_name", "HealthyFresh - Chicken raw dog food")
+           .put("product_price", 9.99));
 
-// Semantic/Vector Search
-float[] embeddings     = new float[] {1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f};
-Filter metadataFilter = new Filter().where("product_price").isEqualsTo(9.99);
-int maxRecord = 10;
-Stream<JsonResult> resultsSet = demoCollection.findVector(embeddings, metadataFilter, maxRecord);
-}
+    // Perform a similarity search
+    float[] embeddings = new float[] {1f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f};
+    Filter metadataFilter = new Filter().where("product_price").isEqualsTo(9.99);
+    int maxRecord = 10;
+    Stream<JsonResult> resultsSet = demoCollection.findVector(embeddings, metadataFilter, maxRecord);
+  }
 }
