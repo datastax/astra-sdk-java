b3c9c96b57a55348bbe7dc2217de173093ea7823
Daniel Sauble <djsauble@gmail.com>
2023-12-15 09:02:15 -0800

    Clean up comments and parameter names in Connecting.java

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/Connecting.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/Connecting.java
@@ -1,20 +1,17 @@
 package com.dtsx.astra.sdk.documentation;
 
 import com.dtsx.astra.sdk.AstraDB;
-
 import java.util.UUID;
 
 public class Connecting {
     public static void main(String[] args) {
-        // Default Initialization
+        // Default initialization
         AstraDB db = new AstraDB("<token>", "<api_endpoint>");
 
-        // --- Other Initializations ---
+        // Initialize with a non-default keyspace
+        AstraDB db1 = new AstraDB("<token>", "<api_endpoint>", "<keyspace>");
 
-        // (1) using non-default keyspace
-        AstraDB db1 = new AstraDB("<token>", "<api_endpoint>", "<keyspac_name>e");
-
-        // (2) using identifier instead of endpoint (regions LB
+        // Initialize with an identifier instead of an endpoint
         UUID databaseUuid = UUID.fromString("<database_id>");
         AstraDB db2 = new AstraDB("<token>", databaseUuid);
     }
