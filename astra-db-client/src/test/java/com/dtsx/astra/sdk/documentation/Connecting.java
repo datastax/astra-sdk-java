ba81087d57341ec12a7583cf69865cef5ca37758
Daniel Sauble <djsauble@gmail.com>
2023-12-15 09:06:10 -0800

    Use the standard two-space indent for Java

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/Connecting.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/Connecting.java
@@ -4,15 +4,15 @@ import com.dtsx.astra.sdk.AstraDB;
 import java.util.UUID;
 
 public class Connecting {
-    public static void main(String[] args) {
-        // Default initialization
-        AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+  public static void main(String[] args) {
+    // Default initialization
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
 
-        // Initialize with a non-default keyspace
-        AstraDB db1 = new AstraDB("<token>", "<api_endpoint>", "<keyspace>");
+    // Initialize with a non-default keyspace
+    AstraDB db1 = new AstraDB("<token>", "<api_endpoint>", "<keyspace>");
 
-        // Initialize with an identifier instead of an endpoint
-        UUID databaseUuid = UUID.fromString("<database_id>");
-        AstraDB db2 = new AstraDB("<token>", databaseUuid);
-    }
+    // Initialize with an identifier instead of an endpoint
+    UUID databaseUuid = UUID.fromString("<database_id>");
+    AstraDB db2 = new AstraDB("<token>", databaseUuid);
+  }
 }
