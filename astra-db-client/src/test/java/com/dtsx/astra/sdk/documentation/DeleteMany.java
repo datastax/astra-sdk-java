ea14147f07c07e5e8de55e0f23a6336785c662ce
Daniel Sauble <djsauble@gmail.com>
2023-12-15 10:36:07 -0800

    Update DeleteMany.java
    If a line is close to 80 characters, don't wrap it.

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/DeleteMany.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/DeleteMany.java
@@ -8,8 +8,7 @@ import io.stargate.sdk.json.domain.DeleteQuery;
 public class DeleteMany {
   public static void main(String[] args) {
     AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-    AstraDBCollection collection = db
-        .createCollection("collection_vector1", 14);
+    AstraDBCollection collection = db.createCollection("collection_vector1", 14);
 
     // Delete items from an existing collection with a query
     int deletedCount = collection
