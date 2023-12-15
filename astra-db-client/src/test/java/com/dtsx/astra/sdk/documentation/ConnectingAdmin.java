2504dead1d06e20b15b3f5e24b3d51b3e2a92109
Daniel Sauble <djsauble@gmail.com>
2023-12-15 10:17:40 -0800

    Tidy the comments in ConnectingAdmin.java

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ConnectingAdmin.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ConnectingAdmin.java
@@ -2,18 +2,15 @@ package com.dtsx.astra.sdk.documentation;
 
 import com.dtsx.astra.sdk.AstraDB;
 import com.dtsx.astra.sdk.AstraDBAdmin;
-
 import java.util.UUID;
 
 public class ConnectingAdmin {
-    public static void main(String[] args) {
-        // Default Initialization
-        AstraDBAdmin client = new AstraDBAdmin("<token>");
+  public static void main(String[] args) {
+    // Default Initialization
+    AstraDBAdmin client = new AstraDBAdmin("<token>");
 
-        /*
-         * You can omit the token if you defined the environment variable
-         * `ASTRA_DB_APPLICATION_TOKEN` or you if are using the Astra CLI.
-         */
-        AstraDBAdmin defaultClient=new AstraDBAdmin();
-    }
+    // You can omit the token if you defined the `ASTRA_DB_APPLICATION_TOKEN`
+    // environment variable or if you are using the Astra CLI.
+    AstraDBAdmin defaultClient=new AstraDBAdmin();
+  }
 }
