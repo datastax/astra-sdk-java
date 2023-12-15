429d7616866c8739835e0ae7d1389029775d6e9b
Daniel Sauble <djsauble@gmail.com>
2023-12-15 11:30:22 -0800

    Update FindCollection.java
    * Two-space indent
    * Proper spacing between package and import statements
    * Remove non-core comments

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/FindCollection.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/FindCollection.java
@@ -1,19 +1,17 @@
 package com.dtsx.astra.sdk.documentation;
+
 import com.dtsx.astra.sdk.AstraDB;
 import io.stargate.sdk.json.domain.CollectionDefinition;
-
 import java.util.Optional;
 
 public class FindCollection {
   public static void main(String[] args) {
-
-    // Given an active db
     AstraDB db = new AstraDB("<token>", "<api_endpoint>");
 
     // Find a collection
     Optional<CollectionDefinition> collection = db.findCollection("collection_vector1");
 
-    // Another test with a collection that does not exist
+    // Verify if a collection exists
     boolean collectionExists = db.isCollectionExists("collection_vector2");
   }
 }
