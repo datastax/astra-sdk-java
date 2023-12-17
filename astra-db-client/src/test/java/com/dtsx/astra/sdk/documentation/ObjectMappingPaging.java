b16986e18bea43da6ac1fde68d3cbb496c9995f2
Daniel Sauble <djsauble@gmail.com>
2023-12-17 12:41:52 -0800

    Update ObjectMappingPaging.java

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ObjectMappingPaging.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ObjectMappingPaging.java
@@ -8,22 +8,18 @@ import io.stargate.sdk.json.domain.Filter;
 import io.stargate.sdk.json.domain.odm.Result;
 
 public class ObjectMappingPaging {
- static class Product {
-     @JsonProperty("product_name")
-     private String name;
-     @JsonProperty("product_price")
-     private Double price;
- }
- public static void main(String[] args) {
+  static class Product {
+    @JsonProperty("product_name") private String name;
+    @JsonProperty("product_price") private Double price;
+  }
+  public static void main(String[] args) {
     AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-    AstraDBRepository<Product> productRepository = db
-      .createCollection("collection_vector1", 14, Product.class);
+    AstraDBRepository<Product> productRepository =
+        db.createCollection("collection_vector1", 14, Product.class);
 
     // Retrieve page 1 of a search (up to 20 results)
     float[] embeddings = new float[]{1f, 0f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f};
     Filter metadataFilter = new Filter().where("product_price").isEqualsTo(9.99);
     Page<Result<Product>> page1 = productRepository.findVector(embeddings, metadataFilter);
-
   }
-
 }
