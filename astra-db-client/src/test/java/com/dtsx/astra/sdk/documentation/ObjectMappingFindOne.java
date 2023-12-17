68157aa2ae2fd5bf77126bd5bb0e2d35f059707d
Daniel Sauble <djsauble@gmail.com>
2023-12-17 11:48:47 -0800

    Update ObjectMappingFindOne.java
    Update indentation and remove superfluous empty lines.

--- astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ObjectMappingFindOne.java
+++ astra-db-client/src/test/java/com/dtsx/astra/sdk/documentation/ObjectMappingFindOne.java
@@ -4,25 +4,24 @@ import com.dtsx.astra.sdk.AstraDB;
 import com.dtsx.astra.sdk.AstraDBRepository;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.stargate.sdk.json.domain.odm.Result;
-
 import java.util.Optional;
 
 public class ObjectMappingFindOne {
- static class Product {
-   @JsonProperty("product_name") private String name;
-   @JsonProperty("product_price") private Double price;
- }
+  static class Product {
+    @JsonProperty("product_name") private String name;
+    @JsonProperty("product_price") private Double price;
+  }
 
-public static void main(String[] args) {
-   AstraDB db = new AstraDB("<token>", "<api_endpoint>");
-   AstraDBRepository<Product> productRepository = db
-      .createCollection("collection_vector1", 14, Product.class);
+  public static void main(String[] args) {
+    AstraDB db = new AstraDB("<token>", "<api_endpoint>");
+    AstraDBRepository<Product> productRepository =
+        db.createCollection("collection_vector1", 14, Product.class);
 
-   // Retrieve a products from its id
-   Optional<Result<Product>> res1 = productRepository.findById("id1");
+    // Retrieve a products from its id
+    Optional<Result<Product>> res1 = productRepository.findById("id1");
 
     // Retrieve a products from its vector (id any)
     float[] vector = new float[]{1f, 0f, 1f, 1f, 1f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f};
     Optional<Result<Product>> res2 = productRepository.findByVector(vector);
- }
+  }
 }
